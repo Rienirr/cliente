1385b50bcdf0ca3ad9d76440c51c6903d9228305
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-09-17 18:30:31 +0200

    clase

--- tema2/practica 2/d.js
+++ tema2/practica 2/d.js
@@ -1,1 +1,10 @@
 'use strict';
+function curso(pNombreCurso,pAnyo,pDescripcion, pAlumnado) {
+    return {nombreCurso: pNombreCurso,
+    anyo: pAnyo,
+    descripcion: pDescripcion,
+    alumnado: pAlumnado,
+    }
+  }
+  var daw= curso("2ºDAW",2021,"Desarrollo aplicaciones WEB",["antonio","pepito"]);
+  console.log(daw);
--- tema2/practica 2/e.js
+++ tema2/practica 2/e.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function curso(pNombreCurso,pAnyo,pDescripcion, pAlumnado) {
+    return {nombreCurso: pNombreCurso,
+    anyo: pAnyo,
+    descripcion: pDescripcion,
+    alumnado: pAlumnado,
+    mostrarCurso: function(){// mostrar específico (sabiendo sus parámetros)
+        return console.log(`El nombre del curso es: ${this.nombreCurso}, el año de desarrollo es ${this.anyo}, 
+        con la descripcion ${this.descripcion} y con los alumnos matriculados: ${this.alumnado}` );
+    }
+    
+    }
+  }
+   function mostrarCualquiera (objeto){// mostrar objeto genérico
+    for(const propiedad in objeto){
+       return console.log(`${propiedad} : ${objeto[propiedad]} `);
+    }
+  
+}
+  const  daw2= curso("2ºDAW",2021,"Desarrollo aplicaciones WEB",["antonio","pepito"]);
+  const daw1=curso("1ºDAW",2020,"Desarrollo aplicaciones WEB",["Goku","Vegeta"]);
+
+  
+  console.log(daw2.mostrarCurso());
+  console.log(daw1.mostrarCurso());
+  console.log(mostrarCualquiera(daw2));
