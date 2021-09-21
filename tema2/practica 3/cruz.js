1e0df0fb26c78e8c0865d66c51573d7e7c1373da
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-09-21 20:39:48 +0200

    final

--- tema2/practica 3/cruz.js
+++ tema2/practica 3/cruz.js
@@ -4,19 +4,20 @@ function Cruz(num){
     let fincruz= cruz*2;
     let hastag="#";
     let punto =".";
-    
+    let linea="";
     for(let i=0;i<num;i++){
+       
         for(let k=0;k<num;k++){
-            let linea="";
+           let linea="";
             if(i<cruz || i>fincruz){
                 linea+=hastag;
             }
             else{
                 linea+=punto;
             }
-            
+           
         }
-        
+        console.log(linea);
     }
 }
 console.log(Cruz(21));
