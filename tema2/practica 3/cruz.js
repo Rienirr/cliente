b7795886c8c17609dbfc9115741a2ac90a86d7d0
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-09-22 10:40:19 +0200

    final 2_3

--- tema2/practica 3/cruz.js
+++ tema2/practica 3/cruz.js
@@ -1,23 +1,43 @@
 "use strict";
-function Cruz(num){
+
+
+
+function cruz(num){
+    let array  = new Array(num);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = new Array(num);
+    }
+    
     let cruz =num/3;
     let fincruz= cruz*2;
     let hastag="#";
     let punto =".";
-    let linea="";
+    
     for(let i=0;i<num;i++){
-       
-        for(let k=0;k<num;k++){
-           let linea="";
-            if(i<cruz || i>fincruz){
-                linea+=hastag;
+    
+        for(let j=0;j<num;j++){
+        
+            if((i<cruz || i>= fincruz) && (j<cruz || j>= fincruz))  {
+                array[i][j]=hastag;
             }
             else{
-                linea+=punto;
+                array[i][j]=punto;
             }
            
         }
-        console.log(linea);
+        
     }
+    
+    let linea="";
+    for(let i=0; i<array.length; i++) {
+      for(let j=0; j<array[i].length; j++) {
+        linea = linea + array[i][j];
+        
+      }
+      console.log(linea);
+      linea="";
+    }
+    
 }
-console.log(Cruz(21));
+console.log(cruz(24));
--- tema2/practica 3/marco.js
+++ tema2/practica 3/marco.js
@@ -1,1 +1,41 @@
 "use strict";
+function marco(num){
+    let array  = new Array(num);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = new Array(num);
+    }
+    
+    let cruz =num/3;
+    let fincruz= cruz*2;
+    let hastag="#";
+    let punto =".";
+    
+    for(let i=0;i<num;i++){
+    
+        for(let j=0;j<num;j++){
+        
+            if((i>=cruz && i<fincruz) &&(j>=cruz && j<fincruz)){
+              array[i][j]=punto;
+            }
+            else{
+              array[i][j]=hastag;
+                
+            }
+           
+        }
+        
+    }
+    
+    let linea="";
+    for(let i=0; i<array.length; i++) {
+      for(let j=0; j<array[i].length; j++) {
+        linea = linea + array[i][j];
+        
+      }
+      console.log(linea);
+      linea="";
+    }
+    
+}
+console.log(marco(18));
