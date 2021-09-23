612301f61b031211255a3218239d6c6d77bbf5de
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-09-23 15:27:33 +0200

    mina

---
+++ tema2/practica 4/sudoku/su.js
@@ -0,0 +1,8 @@
+"use strict";
+let sudoku= [[8],[8],[8],[8],[8],[8],[8],[8],[8]];
+for(let i=0 ;i<sudoku.length;i++){
+    for(let j=0 ;j<sudoku.length;j++){// da igual que length usemos al ser cuadro ambos arrays tienen la misma longitud
+
+    }
+
+}
