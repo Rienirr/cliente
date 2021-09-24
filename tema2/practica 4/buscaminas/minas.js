e6f31523a9637f60dc0a98a9917b9cae4637603f
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-09-24 17:14:21 +0200

    buscaminas

--- tema2/practica 4/buscaminas/minas.js
+++ tema2/practica 4/buscaminas/minas.js
@@ -1,1 +1,28 @@
 'use strict';
+
+
+let minas=[[-1, 0, 0, 0],
+[ 0, 0, 0, 0],
+[ 0,-1, 0, 0],
+[ 0, 0, 0, 0],];
+function tableroCliente(minas){
+    let contadorMinasCercanas=0
+        for(let i=0;i<minas.length;i++ ){
+        for(let j=0;j<minas.length;j++ ){
+            contadorMinasCercanas=0//para reiniciarlo en cada iteración
+            if(minas[i][j]!=1){
+               for(let k=i-1;k<=i+1;k++){
+                for(let z=0;z<3;z++ ){
+                  
+                    if(typeof minas[k][i] == 'undefined'){continue;
+               if (minas[k][i]==1)   contadorMinasCercanas++;
+                 } }
+               }
+            minas[i][j]=contadorMinasCercanas;
+            }
+        }
+    }
+}
+        
+  // if(minas[i-1][j-1]==1 ||minas[i-1][j]==1 ||minas[i-1][j+1]==1 ||minas[i][j-1]==1 ||minas[i][j+1]==1 || minas[i-1][j-1]==1 ||minas[i-1][j]==1 ||minas[i-1][j+1]==1){}
+console.log(tableroCliente(minas));
--- tema2/practica 4/sudoku/su.js
+++ tema2/practica 4/sudoku/su.js
@@ -1,6 +1,5 @@
 "use strict";
-let contadorvertical=1;
-
+let contadorvertical = 1;
 
 let sudoku = [];
 sudoku[0] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -13,81 +12,84 @@ sudoku[6] = [2, 3, 1, 5, 6, 4, 8, 9, 7];
 sudoku[7] = [8, 9, 7, 2, 3, 1, 5, 6, 4];
 sudoku[8] = [5, 6, 4, 8, 9, 7, 2, 3, 1];
 
-
-let vertical=[0, 0, 0, 0, 0, 0, 0, 0, 0];
-let horizontal=[0, 0, 0, 0, 0, 0, 0, 0, 0];
-let  subcuadriculasIzq=[];
-    let subcuadriculasCentrales=[];
-    let subcuadriculasDerecha=[];
-function comprobarsudoku(sudoku){
-    let mensaje="El sudoku esta correcto";
-for(let i=0 ;i<9;i++){
-   
-    
-    if(i==3 ||i==6){//Con esto podemos comprobar las subcuadriculas 3x3 por zonas la izq(las 3 que hay), centro(igual) y der(igual)
-        subcuadriculasIzq=[];
-        subcuadriculasCentrales=[];
-        subcuadriculasDerecha=[];
+let vertical = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let horizontal = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let subcuadriculasIzq = [];
+let subcuadriculasCentrales = [];
+let subcuadriculasDerecha = [];
+function comprobarsudoku(sudoku) {
+  let mensaje = "El sudoku esta correcto";
+  for (let i = 0; i < 9; i++) {
+    if (i == 3 || i == 6) {
+      //Con esto podemos comprobar las subcuadriculas 3x3 por zonas la izq(las 3 que hay), centro(igual) y der(igual)
+      subcuadriculasIzq = [];
+      subcuadriculasCentrales = [];
+      subcuadriculasDerecha = [];
     }
-    
-    for(let j=0 ;j<sudoku.length;j++){// da igual que length usemos al ser cuadro ambos arrays tienen la misma longitud
-        
-            if(j<=2){
-            subcuadriculasIzq.push(sudoku[i][j]);
-            }
-                else if(j<=5){
-                 subcuadriculasCentrales.push(sudoku[i][j]);
-                    }
-            else{
-                subcuadriculasDerecha.push(sudoku[i][j]);
-            }
-            if((j==2&& i==2) || (j==2 && i==5)|| (j==2&& i==8)){//Para hacer las 3 comprobaciones de las subcuadriculas izq 
-                subcuadriculasIzq.sort();
-        console.log(subcuadriculasIzq);
-        for(let k=0;k<9;k++){
-            if(subcuadriculasIzq[k]!=k+1 ){
-            mensaje="Hay algún fallo en sudoku";
-            }
+
+    for (let j = 0; j < sudoku.length; j++) {
+      // da igual que length usemos al ser cuadro ambos arrays tienen la misma longitud
+
+      if (j <= 2) {
+        subcuadriculasIzq.push(sudoku[i][j]);
+      } else if (j <= 5) {
+        subcuadriculasCentrales.push(sudoku[i][j]);
+      } else {
+        subcuadriculasDerecha.push(sudoku[i][j]);
+      }
+      if ((j == 2 && i == 2) || (j == 2 && i == 5) || (j == 2 && i == 8)) {
+        //Para hacer las 3 comprobaciones de las subcuadriculas izq
+        subcuadriculasIzq.sort();
+        //console.log(subcuadriculasIzq);
+        for (let k = 0; k < 9; k++) {
+          if (subcuadriculasIzq[k] != k + 1) {
+            mensaje = "Hay algún fallo en sudoku";
           }
-                
-            }
-            else if ((j==5&& i==2) || (j==5&& i==5)|| (j==5&& i==8)){//Para hacer las 3 comprobaciones de las subcuadriculas centrales comprobamos en vertical 
-               subcuadriculasCentrales.sort();
-               console.log(subcuadriculasCentrales);
-               for(let k=0;k<9;k++){
-                    if(subcuadriculasCentrales[k]!=k+1 ){
-                    mensaje="Hay algún fallo en sudoku";
-                    }
-                  }
-            }
-            else if ((j==8&& i==2) || (j==8&& i==5)|| (j==8&& i==8)){//Para hacer las 3 comprobaciones de las subcuadriculas centrales comprobamos en vertical 
-                subcuadriculasDerecha.sort();
-                console.log(subcuadriculasDerecha);
-                for(let k=0;k<9;k++){
-                    if(subcuadriculasDerecha[k]!=k+1 ){
-                    mensaje="Hay algún fallo en sudoku";
-                    }
-                  }
-                
-            }
-            
-      horizontal[j]=sudoku[i][j];
-      
-      vertical[j]=sudoku[i][j];
-      if(j==8){
-          horizontal.sort();
-        
-          vertical.sort();
-          for(let k=0;k<9;k++){
-            if(horizontal[k]!=k+1 || vertical[k]!=k+1 ){
-            mensaje="Hay algún fallo en sudoku";
-            }
+        }
+      } else if (
+        (j == 5 && i == 2) ||
+        (j == 5 && i == 5) ||
+        (j == 5 && i == 8)
+      ) {
+        //Para hacer las 3 comprobaciones de las subcuadriculas centrales comprobamos en vertical
+        subcuadriculasCentrales.sort();
+      //  console.log(subcuadriculasCentrales);
+        for (let k = 0; k < 9; k++) {
+          if (subcuadriculasCentrales[k] != k + 1) {
+            mensaje = "Hay algún fallo en sudoku";
           }
+        }
+      } else if (
+        (j == 8 && i == 2) ||
+        (j == 8 && i == 5) ||
+        (j == 8 && i == 8)
+      ) {
+        //Para hacer las 3 comprobaciones de las subcuadriculas centrales comprobamos en vertical
+        subcuadriculasDerecha.sort();
+        //console.log(subcuadriculasDerecha); traza para que funcionen subcuadriculas
+        for (let k = 0; k < 9; k++) {
+          if (subcuadriculasDerecha[k] != k + 1) {
+            mensaje = "Hay algún fallo en sudoku";
+          }
+        }
+      }
+
+      horizontal[j] = sudoku[i][j];
+
+      vertical[j] = sudoku[i][j];
+      if (j == 8) {
+        horizontal.sort();
+
+        vertical.sort();
+        for (let k = 0; k < 9; k++) {
+          if (horizontal[k] != k + 1 || vertical[k] != k + 1) {
+            mensaje = "Hay algún fallo en sudoku";
+          }
+        }
       }
     }
-   
-}
+  }
 
-return console.log(mensaje);
+  return console.log(mensaje);
 }
 console.log(comprobarsudoku(sudoku));
