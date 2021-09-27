f3b9fc93bb774b0d8a6e1e2f6b3495af491127a8
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-09-27 15:14:25 +0200

    buscaminas hecho

--- tema2/practica 4/buscaminas/minas.js
+++ tema2/practica 4/buscaminas/minas.js
@@ -10,28 +10,24 @@ function tableroCliente(minas){
         for(let i=0;i<minas.length;i++ ){
         for(let j=0;j<minas.length;j++ ){
             contadorMinasCercanas=0//para reiniciarlo en cada iteración
-      
-            if(minas[i][j]!=-1 && (i!=0 || j!=0 || i!=minas.length-1 || j!=minas.length-1)){//para comprobar que no este en el borde ni en esquinas
+         if(minas[i][j]!=-1){//para comprobar que no este en el borde ni en esquinas
               for(let k=i-1;k<=i+1;k++){
                 for(let z=j-1;z<=j+1;z++ ){
+                  if((k>=0  && z>=0 && k<=minas.length-1  && z<=minas.length-1)) {
                   if (minas[k][z]==-1)   contadorMinasCercanas++;
                      }
               }
 
               }
-              else if(minas[i][j]!=-1 &&((i==0 && j==0) ||(i==0 && j==minas.length-1)  ||(i==minas.length-1 && j==minas.length-1)||(i==minas.length-1 && j==0))){//comprobar esquinas 
+             
                
-              }
-              else {//pues si no ni esquina ni centro solo es un borde 
-
-              }
-               
-               minas[i][j]=contadorMinasCercanas;
+              minas[i][j]=contadorMinasCercanas;
             }
+            
         }
-        return minas;
+       
     }
-
-        
+    return minas;
+  }   
   // if(minas[i-1][j-1]==1 ||minas[i-1][j]==1 ||minas[i-1][j+1]==1 ||minas[i][j-1]==1 ||minas[i][j+1]==1 || minas[i-1][j-1]==1 ||minas[i-1][j]==1 ||minas[i-1][j+1]==1){}
 console.log(tableroCliente(minas));
