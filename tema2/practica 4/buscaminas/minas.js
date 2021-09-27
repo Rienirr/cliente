99ca53db6c74826bc19c975cffec6ee0364ed8c9
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-09-27 16:57:39 +0200

    minas con dos arrays

--- tema2/practica 4/buscaminas/minas.js
+++ tema2/practica 4/buscaminas/minas.js
@@ -6,28 +6,25 @@ let minas=[[-1, 0, 0, 0],
 [ 0,-1, 0, 0],
 [ 0, 0, 0, 0],];
 function tableroCliente(minas){
+let dibujominascercanas= Array.from(minas);//para crear copia del array y mostar el dibujo.Así en caso de crear el juego tendríamos los dos tableros bien diferenciados
     let contadorMinasCercanas=0
         for(let i=0;i<minas.length;i++ ){
         for(let j=0;j<minas.length;j++ ){
             contadorMinasCercanas=0//para reiniciarlo en cada iteración
-         if(minas[i][j]!=-1){//para comprobar que no este en el borde ni en esquinas
+         if(minas[i][j]!=-1){
               for(let k=i-1;k<=i+1;k++){
                 for(let z=j-1;z<=j+1;z++ ){
-                  if((k>=0  && z>=0 && k<=minas.length-1  && z<=minas.length-1)) {
+                  if((k>=0  && z>=0 && k<=minas.length-1  && z<=minas.length-1)) {//para comprobar que no este en el borde ni en esquinas
                   if (minas[k][z]==-1)   contadorMinasCercanas++;
                      }
               }
-
               }
-             
-               
-              minas[i][j]=contadorMinasCercanas;
-            }
-            
+              dibujominascercanas[i][j]=contadorMinasCercanas;
+            }  
         }
-       
     }
-    return minas;
+    return  dibujominascercanas;
   }   
-  
-console.log(tableroCliente(minas));
+let minascercanas=tableroCliente(minas);
+minascercanas.forEach(element => console.log(element));
+ // console.log(tableroCliente(minas));
