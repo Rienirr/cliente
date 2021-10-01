f031d0128f3be410642cd27cff8ee0956582caf7
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-10-01 17:05:51 +0200

    casa

--- cliente/tema 3/practica3/4/4.js
+++ cliente/tema 3/practica3/4/4.js
@@ -1,1 +1,15 @@
 "use strict";
+function fehcha(){
+    setTimeout(() => {
+           
+        let hoy= new Date();
+        let hoyFormateado=`${hoy.getUTCDay()}`;
+        return hoyFormateado;
+          
+       }, 10000);
+
+        }
+    
+        let hoy= new Date();
+        let hoyFormateado=`${hoy.getUTCDay()}`;
+        console.log(hoyFormateado);
