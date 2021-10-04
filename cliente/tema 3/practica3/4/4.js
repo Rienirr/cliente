a1b9ffc0c611041ed7b6229ed1840e08eb21f060
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-10-04 09:56:37 +0200

    Update 4.js
    hecho

--- cliente/tema 3/practica3/4/4.js
+++ cliente/tema 3/practica3/4/4.js
@@ -1,15 +1,75 @@
 "use strict";
-function fehcha(){
-    setTimeout(() => {
-           
         let hoy= new Date();
-        let hoyFormateado=`${hoy.getUTCDay()}`;
-        return hoyFormateado;
-          
-       }, 10000);
-
+        let diaSemana=hoy.getDay();
+        let dia;
+        switch(diaSemana){
+            case 0:
+                dia = "Domingo";
+                break;
+            case 1:
+                dia = "Lunes";
+                break;
+            case 2:
+                dia = "Martes";
+                break;
+            case 3:
+                dia = "Miércoles";
+                break;
+            case 4:
+                dia = "Jueves";
+                break;
+            case 5:
+                dia = "Viernes";
+                break;
+            case 6:
+                dia = "Sádabo";
+                break;
+        } 
+        let nMes= hoy.getMonth();
+        let mes;
+        switch(nMes){
+        case 0:
+            mes="Enero";
+          break;
+        case 1:
+            mes="Febero";
+          break;
+        case 2:
+          mes="Marzo";
+          break;
+        case 3:
+          mes="Abril";
+          break;
+        case 4:
+          mes="Mayo";
+          break;
+        case 5:
+          mes="Junio";
+          break;
+        case 6:
+          mes="Julio";
+          break;
+        case 7:
+          mes="Agosto";
+          break;
+        case 8:
+          mes="Septiembre";
+          break;
+        case 9:
+          mes="Octubre";
+          break;
+        case 10:
+          mes="Noviembre";
+          break;
+        case 11:
+          mes="Diciembre";
+          break;
         }
-    
-        let hoy= new Date();
-        let hoyFormateado=`${hoy.getUTCDay()}`;
-        console.log(hoyFormateado);
+        let hoyFormateado=` Hoy es ${dia} día ${hoy.getDate()} de ${mes} del año ${hoy.getFullYear()} y son las ${hoy.getHours()}:${hoy.getMinutes()}`;
+        function fechaActual(){
+            setTimeout(() => {  
+                console.log(hoyFormateado);   
+               }, 1000);
+        
+                }
+            fechaActual();
