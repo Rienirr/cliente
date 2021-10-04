d734688de5d7496aaef1c926ee118c76761cd008
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-10-04 10:14:58 +0200

    Update 5.js
    DNI

--- cliente/tema 3/practica3/5/5.js
+++ cliente/tema 3/practica3/5/5.js
@@ -1,1 +1,14 @@
 "use strict";
+function calcularDni(letra){
+    let letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+    if(letras.indexOf(letra)<=0){//para controlar que sean letras Correctas 
+        console.log("introduce una letra Correcta");
+    }else{
+      let num=  letras.indexOf(letra);
+     for(let i=num;i<=999;i+=23){
+         console.log(`${i}${letras.charAt(num)}`);
+     }
+}
+}
+calcularDni("L");
+
