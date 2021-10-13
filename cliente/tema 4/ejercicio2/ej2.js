0c2f61ec7aeeb6e452877f9cd723244edbe51471
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-10-13 18:23:10 +0200

    ej2 hecho

--- cliente/tema 4/ejercicio2/ej2.js
+++ cliente/tema 4/ejercicio2/ej2.js
@@ -1,1 +1,8 @@
 "use strict";
+function anyadir(){
+    let numero =Math.round(Math.random()*100000) ;//he puesto para que haya más variedad un número grande
+   let elem= document.createElement("li");
+    elem.innerText=numero;
+    document.getElementById("lista").appendChild(elem);//he puesto un ID ya que he visto en los apuntes la forma más óptima de buscar así nos vamos acostumbrando
+   
+    }
