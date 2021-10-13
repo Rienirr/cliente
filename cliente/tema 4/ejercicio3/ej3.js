c88ea5eee740e8daeedc641f2c6379aba4c87a48
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-10-13 19:10:43 +0200

    clase

--- cliente/tema 4/ejercicio3/ej3.js
+++ cliente/tema 4/ejercicio3/ej3.js
@@ -1,1 +1,35 @@
 "use strict";
+
+function temporizador(){
+    let d= document;
+   let segundos=parseInt(d.getElementById("segundos").innerText.toString());//para así convertir el número que recogemos por html a number
+   let minutos= parseInt(d.getElementById("minutos").innerText.toString());//para así convertir el número que recogemos por html a number
+let elem=d.createElement("p");
+
+    try{
+        if (segundos<0 || segundos>59 || minutos<0 || minutos>59 || typeof segundos!=="number" || typeof minutos!=="number" ){
+            throw "introduce los nuneros correctamente los minutos y segundo deben estar entre 0 y 59 ";
+        } var tempo = setInterval(() => {
+            if (minutos<0 ) {
+                elem.innerText="Se ha acabado el tiempo";//ponemos lo mismo que en el ejercicio anterior 
+               // console.log("Se ha acabado el tiempo");
+               clearInterval(tempo);
+            }
+            else{
+                elem.innerText= ` ${minutos}:${segundos}`
+                //console.log(`El tiempo restante son ${minutos} minutos y ${segundos} segundos `);
+                segundos--;
+                if(segundos==0  ){
+                  segundos= 59;
+                  minutos--;
+                };
+            }
+            d.getElementById("temporizador").appendChild(elem); //añadimos el temporizador
+         }, 1000);
+      
+      }catch(err){
+          console.log(err);
+      }
+    
+      
+    }
--- cliente/tema 4/ejercicio4/ej4.js
+++ cliente/tema 4/ejercicio4/ej4.js
@@ -1,1 +1,21 @@
 "use strict";
+function toCani(){
+let prueba= document.getElementsByTagName("input")[0].innerText;
+    let cani="";
+    do{
+        prueba=  prueba.replace('c', 'k') ;// para que me cambie las c k mientras haya
+       } while(prueba.search("c")>1);
+       
+       for(let i=0;i<prueba.length;i++){
+          if(i%2==0){
+              
+        cani+= prueba.charAt(i).toLocaleUpperCase();  
+           }else {
+               cani+= prueba.charAt(i).toLocaleLowerCase();  // para asegurarnos que son minúsculas. El usuario por definición es idiota e igual nos pasa un string en mayúscula con esto lo tenemos controlado
+       }
+       
+}
+cani+="HHH";
+       return cani;
+
+}
