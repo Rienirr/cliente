211d1f042d8852364fd87ec5377ab48fb0fdb7d0
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-10-14 18:10:46 +0200

    clase

---
+++ cliente/tema 4/practica4_2/1.js
@@ -0,0 +1,7 @@
+"use strict";
+var textoReemplazado=document.body.innerHTML;
+do{
+ (textoReemplazado.replace("sexo","Contenido BLOQUEADO"));
+ console.log(textoReemplazado);
+ } //while(textoReemplazado.search("sexo")==-1);
+   
