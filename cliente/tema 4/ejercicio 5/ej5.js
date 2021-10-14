d0243ba079df827b12ebec2beba481598a539d68
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-10-14 11:05:22 +0200

    lista

--- cliente/tema 4/ejercicio 5/ej5.js
+++ cliente/tema 4/ejercicio 5/ej5.js
@@ -1,1 +1,6 @@
-"use strict";
+
+'use strict';
+function insertAfter(nuevoElemento, elementoExistente) {
+    elementoExistente.parentNode.insertBefore(nuevoElemento, elementoExistente.nextSibling);
+ 
+}
