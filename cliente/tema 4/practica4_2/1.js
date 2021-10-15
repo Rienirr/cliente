77b1b7b2eb0adee6b14b80f88b9a73d8b0b1e096
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-10-15 10:58:09 +0200

    arreglo 1

--- cliente/tema 4/practica4_2/1.js
+++ cliente/tema 4/practica4_2/1.js
@@ -2,5 +2,5 @@
 var d= document;
 var texto=d.body.innerHTML;
 //He encontrado la función replaceAll que funciona como se espera sin usar expresiones regulares 
-var textoReemplazado= texto.replaceAll("sexo","<strong id='rojo'> Contenido bloqueado</strong>");//le añado la id para luego en la hoja de estilos añadir el estilo que nos pide
+var textoReemplazado= texto.replaceAll("sexo","<strong id='rojo'> Contenido Bloqueado</strong>");//le añado la id para luego en la hoja de estilos añadir el estilo que nos pide
 d.body.innerHTML=textoReemplazado;
