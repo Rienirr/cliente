15856c712807ff3a5d2e2d13758b58864952d793
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-10-15 13:14:48 +0200

    casa

--- cliente/tema 4/practica4_2/3.js
+++ cliente/tema 4/practica4_2/3.js
@@ -1,1 +1,10 @@
 "use strict";
+let d=document;
+desaparecer(posicion){//le pasamos la posición del párrafo a la que aplicar el estilo 
+let parrafo =d.getElementsByTagName("p")[posicion];
+parrafo.setAttribute("class","desaparecer");
+}
+eliminar(posicion){//le pasamos la posición del párrafo a la que aplicar el estilo 
+    let parrafo =d.getElementsByTagName("p")[posicion];
+parrafo.setAttribute("class","eliminado");
+}
