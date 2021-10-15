d2b55479d0c9dc0f5bb67420c89d267eecfbedb1
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-10-15 13:36:36 +0200

    listo

--- cliente/tema 4/practica4_2/3.js
+++ cliente/tema 4/practica4_2/3.js
@@ -1,10 +1,19 @@
 "use strict";
 let d=document;
-desaparecer(posicion){//le pasamos la posición del párrafo a la que aplicar el estilo 
+
+function desaparecer(posicion){ //le pasamos la posición del párrafo a la que aplicar el estilo 
 let parrafo =d.getElementsByTagName("p")[posicion];
 parrafo.setAttribute("class","desaparecer");
+
 }
-eliminar(posicion){//le pasamos la posición del párrafo a la que aplicar el estilo 
+function eliminar(posicion){ //le pasamos la posición del párrafo a la que aplicar el estilo
+    
     let parrafo =d.getElementsByTagName("p")[posicion];
 parrafo.setAttribute("class","eliminado");
+}
+function restaurar(){
+    let parrafos =d.getElementsByTagName("p");
+    for (const valor in parrafos) {
+        parrafos[valor].removeAttribute("class");
+}
 }
