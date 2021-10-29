4bad40324bfc5f6c0cf0bdb5d3e03dca2c62ca99
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-10-29 17:03:56 +0200

    listo

--- cliente/tema 5/practica5_1/ejercicio2.js
+++ cliente/tema 5/practica5_1/ejercicio2.js
@@ -1,6 +1,9 @@
 "use strict";
 window.onload= function() {
     var d=document;
+    var p= d.createElement("p");
+    d.body.appendChild(p);
+    p.innerHTML="Pincha para cambiar el fondo";
     function colorRamdon(){//Hacemos que cree 6 números aleatorios entre el 0 y 15  para luego asignarlos al array que nos has dado
         let color="#";
         for(let i=0;i<6;i++) {
