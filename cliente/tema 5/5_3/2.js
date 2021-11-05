401aa36718086c57de1f9299d16bb080333b7fd5
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-11-05 16:56:15 +0100

    casa

--- cliente/tema 5/5_3/2.js
+++ cliente/tema 5/5_3/2.js
@@ -3,17 +3,40 @@
 window.onload = () => {
 let d= document;
 let div= d.createElement("div");
+function crearComunicacionUsuario(){//este método lo usaremos siempre para comunicarnos con el usuario.
+ let mostrar = d.createElement("div");
+       mostrar.innerHTML="<p id='mensajeAlUsuario' class='hidden'> Listopan pon algo en textarea y selecciona una opción</p>"
+d.body.appendChild(mostrar);
+}
+crearComunicacionUsuario();
 //Creamos el formulario de forma dinámica.
-div.innerHTML= "<form > <fieldset> <legend>Formulario </legend><textarea> </textarea> <select> <option value='claro'>Fondo claro</option><option value='oscuro'>Fondo oscuro</option> <option value='degradado'>Degradado</option></select> <p id='crear'>Crear parrafo</p>  </fieldset> </form>"
+div.innerHTML= "<form > <fieldset> <legend>Formulario </legend><textarea> </textarea> <select id='op'> <option value='cero'>Seleciona una de las siguientes opciones</option> <option value='claro'>Fondo claro</option><option value='oscuro'>Fondo oscuro</option> <option value='degradado'>Degradado</option></select> <input type='button' id='crear' value='Crear parrafo'>  </fieldset> </form>"
 d.body.appendChild(div);
-let boton= d.getElementsByTagName("p")[0];
+let boton= d.getElementById("crear");
  boton.addEventListener("click", (e)=>{//
+
+       let posicion=d.getElementById("op").selectedIndex;
+       let select=d.getElementById("op");
+       let estilo= select.options[posicion].value
         let valor=(d.getElementsByTagName("textarea")[0].value);
-        
+        if (valor.trim().length == 0 || estilo == "cero" ){// Valoramos para que en caso de error nos muestre un mensaje por pantalla 2 segundos.
+               let mensajeError= d.getElementById("mensajeAlUsuario");
+               mensajeError.classList.remove("hidden");
+               setTimeout(()=>{
+                     mensajeError.classList.add("hidden");
+               }, 2000)
+              
+        }
+        else{//Si no hay error hacemos lo que pide el programa.
+
+
+      
         let di=d.createElement("div");
-        di.innerHTML=`<p> ${valor}</p> `;
+        di.innerHTML=`<p class="${estilo}"> ${valor}</p> `;
         d.forms[0].insertAdjacentElement("afterend", di);
         d.getElementsByTagName("textarea")[0].value="";
+       select.selectedIndex=0;// Para poner el valor a 0 y cada vez tengamos que elegir las opciones.
+       }
  }, false);
 
 
--- cliente/tema 5/5_3/3.js
+++ cliente/tema 5/5_3/3.js
@@ -1,5 +1,61 @@
 "use strict";
 
 window.onload = () => {
-    
+    let d= document;
+    let boton= d.getElementById("guardar");
+    function crearComunicacionUsuario(){//este método lo usaremos siempre para comunicarnos con el usuario.
+        let mostrar = d.createElement("div");
+              mostrar.innerHTML="<p id='mensajeAlUsuario' class='hidden'> Tienes que rellenar todos los campos melón y en los el año de publicación tiene que ser de 1901 a 2021(presente) y en la localización un número mayor o igual a 0</p>"
+       d.body.appendChild(mostrar);
+       }
+       crearComunicacionUsuario();
+
+    boton.addEventListener("click",(e)=>{
+        let disco= d.getElementById("disco");
+        let grupo=  d.getElementById("grupo");
+        let anyo= d.getElementById("anyo");
+        let cbox1= d.getElementById("rock");
+        let cbox2= d.getElementById("progre");
+        let cbox3= d.getElementById("punk");
+        let cbox4= d.getElementById("trash");
+        let estanteria= d.getElementById("est");
+        let radio = document.getElementsByName("prestado");
+        
+       
+
+    if(disco.value.trim().length == 0 || grupo.value.trim().length==0 ||anyo.value.trim().length==0 ||  parseInt(anyo.value)<1900 ||  parseInt(anyo.value)>=2022 || estanteria.value.trim().length==0|| parseInt(estanteria.value)<=0 ){
+        let mensajeError= d.getElementById("mensajeAlUsuario");
+        mensajeError.classList.remove("hidden");
+        setTimeout(()=>{
+              mensajeError.classList.add("hidden");
+        }, 5000)
+    }
+    else{
+        let div= d.createElement("div");
+        div.setAttribute("class", "listado");
+       let mensajePrestado;
+       let mensajeLocalizacion="";
+       let generosDelDisco=" los generos que tiene el disco son";
+        if(radio[0].checked=== true){
+            mensajePrestado="prestado";
+        }
+        else{
+           
+            mensajePrestado="disponible";
+            mensajeLocalizacion= `y se encuentra en la estantería ${estanteria.value}`;//Así solo muestra la información de la estantería si se encuentra disponible.
+        }
+        if(cbox1.checked=== true) generosDelDisco+= ` ${cbox1.value}`;
+        if(cbox2.checked=== true) generosDelDisco+= ` ${cbox2.value}`;
+        if(cbox3.checked=== true) generosDelDisco+= ` ${cbox3.value}`;
+        if(cbox4.checked=== true) generosDelDisco+= ` ${cbox4.value}`;;
+        if( generosDelDisco===" los generos que tiene el disco son" ){
+            generosDelDisco="";//puesto que no se ha añadido nada
+        }
+
+        div.innerHTML=` <p> El disco: ${disco.value} creado por ${grupo.value} se lanzó en el año ${anyo.value} y se encuentra: ${mensajePrestado}${generosDelDisco} ${mensajeLocalizacion} </p>`;
+        d.body.appendChild(div);
+
+
+    }
+    }, false);
 }
