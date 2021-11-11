439515f0f30bd78216d05efdaca40291aebbadad
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-11-11 16:54:59 +0100

    clase

--- cliente/tema 5/5_3/6.js
+++ cliente/tema 5/5_3/6.js
@@ -1,19 +1,12 @@
-"use strict";
-
-
-    
+"use strict";  
 window.onload = () => {
     var generosDelDisco=" los generos que tiene el disco son";
     var d= document;
-   
     var form= d.getElementsByTagName("form")[0];
     var boton= d.getElementById("guardar");
- 
     let divListado= d.createElement("div");
-
     var ListadoDeDiscos=[];
-    function discoCompleto(pDisco, pGrupo, pAnyo, pMensajePrestado, pGenerosDelDisco, pMensajeLocalizacion){
-           
+    function discoCompleto(pDisco, pGrupo, pAnyo, pMensajePrestado, pGenerosDelDisco, pMensajeLocalizacion){       
         return {
        disco: pDisco,
        grupo: pGrupo,
@@ -44,41 +37,43 @@ window.onload = () => {
         var elementoAnyo=d.getElementById("anyo");
 
         elementoAnyo.className="";
+        d.getElementsByTagName("fieldset")[1].className="";
         var cbox1= d.getElementById("rock");
         var cbox2= d.getElementById("progre");
         var cbox3= d.getElementById("punk");
         var cbox4= d.getElementById("trash");
         var estanteria= d.getElementById("est");
+      
         estanteria.className="";
         var radio = document.getElementsByName("prestado");
         
        
         let patronDiscoyGrupo = /^[a-zA-Z]{4,}[\s+[a-zA-Z]{1,}]*/;
         let patronAnyo=/^[0-9]{4}$/;
+        let patronEstanteria=/^[E]{1}[S]{1}[-]{1}[0-9]{3}[A-Z]{2}$/;//si queremos ponerlo que de igual mayúsculas ponemos i al final para que sea insensitive
+        let estanteriaComprobacion= patronEstanteria.test(estanteria.value);
         let discoComprobacion= patronDiscoyGrupo.test(disco);
         let grupoComprobacion=patronDiscoyGrupo.test(grupo);
         let anyoComprobaccion=patronAnyo.test(anyo);
         
-    if(!discoComprobacion){//Comprobación de disco.
+    if(!discoComprobacion ){//Comprobación de disco sale mal ponemos la error en el resto hacemos lo mismo.
      
         elementoDisco.setAttribute("class","error");
         correcto=false;
     }
-    if(!grupoComprobacion){//Comprobación de grupo.
+    if(!grupoComprobacion ){//Comprobación de grupo.
        
         elementoGrupo.setAttribute("class","error");
         correcto=false;
-    }//Comprobaciones de 
+    }
    
-    if(!anyoComprobaccion){
+    if(!anyoComprobaccion || parseInt(anyo)<1900  || parseInt(anyo)>=2022){//Comprobamos además que el año sea desde el 1900 hasta el presente
       
         elementoAnyo.setAttribute("class","error");
         correcto=false;
     }
-    if(estanteria.value.trim().length  <4){
-        //comparar bien con expresiones regulares
-        let patron= /^[0-9]{4}$/;
-        estanteria.setAttribute("class","error");
+    if(!estanteriaComprobacion){
+     estanteria.setAttribute("class","error");
         correcto=false;
     }
     if(cbox1.checked=== true) generosDelDisco+= ` ${cbox1.value}`;
@@ -110,17 +105,14 @@ window.onload = () => {
             
            
         divListado.innerHTML="";
-            for(let i=0; i<ListadoDeDiscos.length; i++){
-             
+            for(let i=0; i<ListadoDeDiscos.length; i++){     
                 let p=d.createElement("p");
                 p.innerHTML=`El disco: ${ListadoDeDiscos[i].disco} creado por ${ListadoDeDiscos[i].grupo} se lanzó en el año ${ListadoDeDiscos[i].anyo} y se encuentra: ${ListadoDeDiscos[i].mensajePrestado}${ListadoDeDiscos[i].generosDelDisco} ${ListadoDeDiscos[i].mensajeLocalizacion} </p>`;
                 divListado.appendChild(p);
                 
-            }
-          
+            } 
        d.body.appendChild(divListado);
-       
-           
+        
         }
         else{
             let mensajeError= d.getElementById("mensajeAlUsuario");
@@ -129,13 +121,6 @@ window.onload = () => {
                   mensajeError.classList.add("hidden");
             }, 5000)
         }
-      
-       
-       
-        
-
-
-      
     
     }, false);
 }
--- cliente/tema 5/5_3/7.js
+++ cliente/tema 5/5_3/7.js
@@ -15,34 +15,84 @@ window.onload = () => {
    var inputCheckBox=d.getElementsByTagName("input")[5];
    var inputRadio=d.getElementsByTagName("input")[6];
    var inputSubmit= d.getElementsByTagName("input")[7];
+   var form= d.createElement("form");
+   form.innerHTML="<legend> Crea un formulario dinámicamente</legend>"
+ 
+   d.body.appendChild(form);
 
+function comprobar(elemento){//creamos una función para 
+   do{
 
+      var inputUsuario= prompt( `¿Cuál es tu ${elemento}?(mínimo 5 caracteres)`);
+      var patron=/^[a-zA-Z]{4,}[\s+[a-zA-Z]{1,}]*/;
+   }while(!patron.test(inputUsuario));
+   return inputUsuario;
+} 
+function comprobarDosVeces(elemento){
+
+   do{
+         
+      var idElemento= prompt(`¿Cuál es la id del ${elemento} a crear?(mínimo 2 caracteres)`);
+      var patron=/^[a-zA-Z]{1}[\s+\w{1,}]*/;
+      var valueElemento=  prompt(`¿Cuál es el value del ${elemento} a crear?(mínimo 2 caracteres)`);
+   }while(!patron.test(idElemento)|| !patron.test(valueElemento));
+   return{
+      id: idElemento,
+      value:valueElemento
+   }
+}
    inputNombre.addEventListener("click",(event)=>{
-
+      var nombre=comprobar("nombre");
+      let input=d.createElement("input");
+      input.setAttribute("type","text");
+      input.setAttribute("id",nombre);
+      form.appendChild(input);
+  
    },false);
 
    inputContrasenya.addEventListener("click",(event)=>{
-
+    
+       var contraseyaa= comprobar("contraseña");
+       var input=d.createElement("input");
+       input.setAttribute("type","contraseña");
+       input.setAttribute("id",contraseyaa);
+       form.appendChild(input);
 },false);
 
 inputTextArea.addEventListener("click",(event)=>{
-
+   
+      var text= comprobar("textArea");
+      var input=d.createElement("textarea");
+   
+       input.setAttribute("id",text);
+       form.appendChild(input);
+      
 },false);
 
-inputLabel.addEventListener("click",(event)=>{
+inputLabel.addEventListener("click",(event)=>{    
+      var label= comprobar("label");
 
 },false);
 
 inputImagen.addEventListener("click",(event)=>{
+  
+       var img= prompt("¿Cuál es la ruta de la imagen?");
+      var ruta;
+   
 
 },false);
 inputCheckBox.addEventListener("click",(event)=>{
+ 
+ var checkBox=comprobarDosVeces("checkbox");
+   let input=
 
 },false);
 inputRadio.addEventListener("click",(event)=>{
+   var radio=comprobarDosVeces("radio button");
 
 },false);
 inputSubmit.addEventListener("click",(event)=>{
-
+  var submit=comprobarDosVeces("submit");
+   inputSubmit.setAttribute("disabled","true");
 },false);
 }
