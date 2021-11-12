0a538bb2f6a43ef55070d2cb5a1fa999f2fe526b
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-11-12 11:57:55 +0100

    cambios

--- cliente/tema 5/5_3/5.js
+++ cliente/tema 5/5_3/5.js
@@ -6,15 +6,19 @@ window.onload = () => {
     segundoSelect.setAttribute("class","hidden");
     d.forms[0].appendChild(segundoSelect);
     let select= d.getElementsByTagName("select")[0];
-    let ciudadesAlicante=["Alicante", "Elche","Petrer"];
-    let ciudadesCastellon=["Castellón", "Oropesa", "Vinazoz"];
-    let ciudadesValencia=["Valencia","Xàtiva","Torrent"];
- 
+    let selectCiudades= d.getElementsByTagName("select")[1];
+    let ciudadesAlicante=["Seleccione una opción válida","Alicante", "Elche","Petrer"];
+    let ciudadesCastellon=["Seleccione una opción válida","Castellón", "Oropesa", "Vinazoz"];
+    let ciudadesValencia=["Seleccione una opción válida","Valencia","Xàtiva","Torrent"];
+    var submit=d.createElement("input");
+    submit.setAttribute("type","submit");
+    submit.setAttribute("class" ,"hidden");
+    d.forms[0].appendChild(submit);
     
     
      select.addEventListener("change", (e)=>{//Con el cambio se ejecuta la función para mostrar los resultados.
     
-     let posicion=d.getElementById("provincia").selectedIndex;
+     let posicion=select.selectedIndex;
            let provincia="false";
          
           if(posicion===1){
@@ -27,7 +31,7 @@ window.onload = () => {
             provincia=ciudadesValencia;
           }
           if(provincia!="false"){
-            segundoSelect.innerHTML=`<option value='${provincia[0]}'>${provincia[0]}</option> <option value='${provincia[1]}'>${provincia[1]}</option>  <option value='${provincia[2]}'>${provincia[2]}</option></select>  `
+            segundoSelect.innerHTML=` <option value='${provincia[0]}'>${provincia[0]}</option> <option value='${provincia[1]}'>${provincia[1]}</option>  <option value='${provincia[2]}'>${provincia[2]}</option>  <option value='${provincia[3]}'>${provincia[3]}</option> `
             segundoSelect.classList.remove("hidden");
           }
           else{//Para ocultar el select si tenemos una opción seleccionada.
@@ -37,9 +41,17 @@ window.onload = () => {
            
            }
      , false);
+     selectCiudades.addEventListener("change", (e)=>{//Así solo puede enviar las consultas si está bien de otra forma no aparece el boton de envío.
     
-    
-    
+      let posicion2=selectCiudades.selectedIndex;
+      console.log(posicion2);
+    if(posicion2===0){
+      submit.setAttribute("class" ,"hidden");
+    }else{
+      submit.setAttribute("class" ,"");
     }
+    
+    }, false);
+  }
     
     
--- cliente/tema 5/5_3/7.js
+++ cliente/tema 5/5_3/7.js
@@ -2,11 +2,15 @@
 
 window.onload = () => {
    var d= document;
-   var table=d.createElement("table");
-   //Creamos la tabla dinámicamente y con los input que nos pide el enunciado
+   function crearComunicacionUsuario(){//Este método lo usaremos siempre para comunicarnos con el usuario.
+      let mostrar = d.createElement("div");
+            mostrar.innerHTML=`<p id='mensajeAlUsuario' class='hidden'>  </p>`;
+     d.body.appendChild(mostrar);
+     }
+     crearComunicacionUsuario();
+   var table=d.createElement("table"); //Creamos la tabla dinámicamente y con los input que nos pide el enunciado
    table.innerHTML=`<tr><td> <input type="button" value="Crear Nombre" ></td>  <td><input type="button" value="Crear Password" ></td> <td > <input type="button" value="CrearTextArea" > </td> <td > <input type="button" value="CrearLabel" ></td> <td> <input type="button" value="Crear Imagen" ></td>  <td><input type="button" value="Crear CheckBox" ></td> <td><input type="button" value="Crear radio button" ></td> <td> <input type="button" value="Crear Submit" ></td> </tr>`;
-   d.body.appendChild(table);
-   //He creado una variable por cada botón para verlo de forma más clara que si pongo input[0].
+   d.body.appendChild(table); //He creado una variable por cada botón para verlo de forma más clara que si pongo input[0].
    var inputNombre =d.getElementsByTagName("input")[0];
    var inputContrasenya =d.getElementsByTagName("input")[1];
    var inputTextArea =d.getElementsByTagName("input")[2];
@@ -15,26 +19,24 @@ window.onload = () => {
    var inputCheckBox=d.getElementsByTagName("input")[5];
    var inputRadio=d.getElementsByTagName("input")[6];
    var inputSubmit= d.getElementsByTagName("input")[7];
-   var form= d.createElement("form");
+   var form= d.createElement("form")
+  form.setAttribute("class","hidden");
    form.innerHTML="<legend> Crea un formulario dinámicamente</legend>"
- 
    d.body.appendChild(form);
 
-function comprobar(elemento){//creamos una función para 
+function comprobar(elemento){//creamos una función para reutilzar cuando necesitamos solo id.
    do{
-
       var inputUsuario= prompt( `¿Cuál es tu ${elemento}?(mínimo 5 caracteres)`);
       var patron=/^[a-zA-Z]{4,}[\s+[a-zA-Z]{1,}]*/;
    }while(!patron.test(inputUsuario));
    return inputUsuario;
 } 
-function comprobarDosVeces(elemento){
+function comprobarDosVeces(elemento){//Otra si necesitamos value y id.
 
-   do{
-         
-      var idElemento= prompt(`¿Cuál es la id del ${elemento} a crear?(mínimo 2 caracteres)`);
-      var patron=/^[a-zA-Z]{1}[\s+\w{1,}]*/;
-      var valueElemento=  prompt(`¿Cuál es el value del ${elemento} a crear?(mínimo 2 caracteres)`);
+   do{  
+      var idElemento= prompt(`¿Cuál es la id del ${elemento} a crear?(mínimo 5 caracteres)`);
+      var patron=/^[a-zA-Z]{4,}[\s+[a-zA-Z]{1,}]*/;
+      var valueElemento=  prompt(`¿Cuál es el value del ${elemento} a crear?(mínimo 5 caracteres)`);
    }while(!patron.test(idElemento)|| !patron.test(valueElemento));
    return{
       id: idElemento,
@@ -43,56 +45,70 @@ function comprobarDosVeces(elemento){
 }
    inputNombre.addEventListener("click",(event)=>{
       var nombre=comprobar("nombre");
-      let input=d.createElement("input");
-      input.setAttribute("type","text");
-      input.setAttribute("id",nombre);
-      form.appendChild(input);
-  
+      form.classList="";//Para que siempre se muestre el formulario escondido.
+     form.innerHTML+= `<input type="text" id="${nombre}" >`; 
    },false);
 
    inputContrasenya.addEventListener("click",(event)=>{
     
        var contraseyaa= comprobar("contraseña");
-       var input=d.createElement("input");
-       input.setAttribute("type","contraseña");
-       input.setAttribute("id",contraseyaa);
-       form.appendChild(input);
+       form.classList="";//Para que siempre se muestre el formulario escondido.
+       form.innerHTML+= `<input type="password" id="${contraseyaa}" >`; 
+      
 },false);
 
 inputTextArea.addEventListener("click",(event)=>{
    
       var text= comprobar("textArea");
-      var input=d.createElement("textarea");
-   
-       input.setAttribute("id",text);
-       form.appendChild(input);
-      
+      form.classList="";//Para que siempre se muestre el formulario escondido.
+      form.innerHTML+= `<textarea < id="${text}" cols="40" rows="5"></textarea>`; 
+     
 },false);
 
-inputLabel.addEventListener("click",(event)=>{    
+inputLabel.addEventListener("click",(event)=>{    //Este método es el más largo ya que necesitamos hacer más cosas buscar el elemento y añadir el label antes para que tenga sentido.
       var label= comprobar("label");
+     if (d.getElementById(label)!=null){
+      var labelAInsertar= d.createElement("label");
+      labelAInsertar.innerText=`${label}:`;
+      labelAInsertar.setAttribute("for",label);
+      d.getElementById(label).insertAdjacentElement("beforebegin",labelAInsertar);
+     }else{
+      let mensajeError= d.getElementById("mensajeAlUsuario");
+      mensajeError.innerHTML="Has de poner un id ya existente";
+      mensajeError.classList.remove("hidden");
+      setTimeout(()=>{
+            mensajeError.classList.add("hidden");
+      }, 5000)
+  
+     };
 
 },false);
 
-inputImagen.addEventListener("click",(event)=>{
+inputImagen.addEventListener("click",(event)=>{//Aquí no se como controlar el error ya que si el usuario puede insertar cualquier imagen de internet no sé como hacer el patrón.
   
        var img= prompt("¿Cuál es la ruta de la imagen?");
-      var ruta;
+       form.classList="";//Para que siempre se muestre el formulario escondido.
+      form.innerHTML+= `<img id="${img}" src="${img}" width="250" ></img>`;
+     
    
 
 },false);
 inputCheckBox.addEventListener("click",(event)=>{
  
  var checkBox=comprobarDosVeces("checkbox");
-   let input=
+ form.classList="";//Para que siempre se muestre el formulario escondido.
+ form.innerHTML+= `<input type="checkbox" id="${checkBox.id}" value="${checkBox.value}" >`; 
 
 },false);
 inputRadio.addEventListener("click",(event)=>{
    var radio=comprobarDosVeces("radio button");
+   form.classList="";//Para que siempre se muestre el formulario escondido.
+   form.innerHTML+= `<input type="checkbox" id="${radio.id}" value="${radio.value}" >`; 
 
 },false);
 inputSubmit.addEventListener("click",(event)=>{
   var submit=comprobarDosVeces("submit");
-   inputSubmit.setAttribute("disabled","true");
+  form.classList="";//Para que siempre se muestre el formulario escondido.
+  form.innerHTML+= `<input type="checkbox" id="${submit.id}" value="${submit.value}" >`
 },false);
 }
