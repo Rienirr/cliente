262f780f7a78b155b351cacde43bbdf844c9b3fe
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-11-29 18:46:27 +0100

    entregado

--- cliente/tema 7/7_1.js
+++ cliente/tema 7/7_1.js
@@ -1,7 +1,7 @@
 import {consulta} from '/imports/starWarsAPI.js ';
 "use strict";
  window.onload= ()=>{
-
+//Consultamos los datos de las películas.
 consulta();
 
  
--- cliente/tema 7/imports/starWarsAPI.js
+++ cliente/tema 7/imports/starWarsAPI.js
@@ -1,5 +1,5 @@
 "use strict";
-export function consulta(){
+export function consulta(){//Hacemos la consulta que se encarga de mostrar las películas.
 const url = "https://swapi.dev/api/films";
 var httpRequest = new XMLHttpRequest();
 httpRequest.open("GET", url, true);
@@ -11,7 +11,7 @@ httpRequest.addEventListener(
   "readystatechange",
   () => {
     
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {//Una vez acabada la consulta mostramos las películas.
       let peliculas=JSON.parse(httpRequest.response); 
       mostrar(peliculas);
     
@@ -19,14 +19,16 @@ httpRequest.addEventListener(
   },
   true
 );
-httpRequest.send(); 
+httpRequest.send(); //Envíamos la consulta.
 
-function mostrar(peliculas) {
+function mostrar(peliculas,id="peliculas") {//Mostramos las películas y añadimos los manejadores de forma dinámica. 
+  //Al tener como parámetro opcional una id luego podemos ponerlo en cualquier otro lugar si nos tenemos que ajustar a las plantilla.
   let cadena = " ";
   peliculas.results.map((v, i, a) => {
     cadena += `<li>(${v.episode_id}) ${v.title} `;
   });
   document.getElementById("peliculas").innerHTML =` <h1> Películas</h1> <ul>${cadena}</ul>`;
+  document.getElementById("peliculas").classList="";
   let listas= document.getElementsByTagName("li");
   console.log(listas);
    for(let i=0;i<listas.length;i++){
@@ -38,13 +40,13 @@ function mostrar(peliculas) {
 
 } 
 }
-function personajes (indice,pelicula){
-   let personaje = document.createElement("p");
+function personajes (indice,pelicula,id ="personajes"){//Muestra 10 personajes de la película indicada.
+  //Al tener como parámetro opcional una id luego podemos ponerlo en cualquier otro lugar si nos tenemos que ajustar a las plantilla.
    let div =document.getElementById("personajes");
-   div.innerHTML=      '<h2>Personajes</h2>' ;
+ //  div.innerHTML=      `<h2>Personajes de ${} </h2>` ;
    pelicula.results.map((v, i, a) => {
        if(i==indice){
-          
+        div.innerHTML=      `<h2>10 Personajes de ${v.title} </h2>` ;
            v.characters.map((v,i,a)=>{
             if(i<10){
               
@@ -56,13 +58,11 @@ function personajes (indice,pelicula){
               );
               httpRequest.addEventListener(
                 "readystatechange",
-                () => {
-                  
+                () => { 
                   if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-                    let actor=JSON.parse(httpRequest.response); 
-                   
-                  div.innerHTML+=` <p>${actor.name}</p>`;
-                  
+                    let actor=JSON.parse(httpRequest.response);                   
+                  div.innerHTML+=` <p>${actor.name}</p>`;  
+                  div.classList="";
                   }
                 },
                 true
@@ -73,12 +73,17 @@ function personajes (indice,pelicula){
        } 
    });
  } 
-function sinopsis(indice,pelicula){
+function sinopsis(indice,pelicula,id="descripcion"){//Muestra la sinopsis de la película indicada.
+  //Al tener como parámetro opcional una id luego podemos ponerlo en cualquier otro lugar si nos tenemos que ajustar a las plantilla.
    let cadena = " ";
    pelicula.results.map((v, i, a) => {
       if(i==indice){
-       cadena+=   v.opening_crawl;
+       cadena+= `<h2> Sinopsis de ${v.title}</h2>   <p>${v.opening_crawl}</p> <img src='images/${i}.jpeg' alt="Star Wars"  width="250"> `;
+       //Las películas estan ordenadas según la Api,es decir, en orden de salida.
+       
       }       
    });
-   document.getElementById("descripcion").innerHTML =` <h2> Sinopsis</h2> ${cadena}`;
-}
+  
+   document.getElementById("descripcion").innerHTML =`  ${cadena}`;
+   document.getElementById("descripcion").classList="";
+    }
