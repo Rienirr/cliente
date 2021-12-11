853fffc51c6427f72b70b162e7f37e59f2708c71
Rienirr <90462305+Rienirr@users.noreply.github.com>
2021-12-11 12:38:51 +0100

    pus

--- cliente/tema 7/practica3/Trivial/trivial.js
+++ cliente/tema 7/practica3/Trivial/trivial.js
@@ -1,34 +1,42 @@
 "use strict";
-import {getTrivial, mostrarPreguntas} from "../importsAPI/TrivialApi.js"
+import {comprobarAcertadas, getTrivial, mostrarPreguntas, vaciar} from "../importsAPI/TrivialApi.js"
 window.onload = ()=>{
     var d= document;
     var numeroPreguntas= d.getElementById("preguntas");
     var dificultad= d.getElementById("dificultad");
     var categoria= d.getElementById("categoria");
     var boton = d.getElementById("empezar");
+    var resultados = d.getElementById("final");
+    var reiniciar = d.getElementById("reiniciar");
     boton.addEventListener("click",()=>{
        if( numeroPreguntas.selectedIndex==0 || dificultad.selectedIndex==0 || categoria.selectedIndex==0 ){
            console.log("error paquete");
         //Mostrar mensaje al usuario de Error.
        }
        else{
+        let div = document.getElementById("mostrar");
+        div.innerHTML="";
         let amount= numeroPreguntas.options[numeroPreguntas.selectedIndex].value;
         let difficulty=dificultad.options[dificultad.selectedIndex].value; 
         let category=categoria.options[categoria.selectedIndex].value; 
         getTrivial(amount,difficulty,category).then((trivial)=>{
             trivial.results.map((v,i,a)=>{//Así recorremos el array de preguntas y luego las mostramos debidamente
-                console.log(v);
                 mostrarPreguntas(v);
             });
+         resultados.classList.remove("hidden");
+         reiniciar.classList.remove("hidden");
         }).catch((error)=>{
-console.log(error.message);
+        console.log(error.message);
         });
-    
-        
        }
-       
-        
-        
-        
     });
+    
+
+    resultados.addEventListener("click",()=>{
+        comprobarAcertadas();
+        
+    },false);
+    reiniciar.addEventListener("click",()=>{
+        vaciar();
+    },false);
 }
--- cliente/tema 7/practica3/importsAPI/TrivialApi.js
+++ cliente/tema 7/practica3/importsAPI/TrivialApi.js
@@ -1,5 +1,7 @@
 "use strict";
 export const getTrivial= (amount,difficulty,category )=>{
+  /*Nos devuelve un objeto json con todas las preguntas que nosotros le pasemos con esos 3 parámetros cantidad, dificultad y categoría.
+  (No he añadido más ya que creo que así hay bastante aleatoriedad pero lo más sencillo añadir más categorías si lo queremos ampliar en un futuro).*/
 
 /* ------DATOS NECESARIOS----
 historia=23 ,deportes=21 y no necesitas nada si son todas las categorías.
@@ -25,22 +27,68 @@ var peticion = new Request(url, {
     });
 };
 
- export function mostrarPreguntas(pregunta){
-   let arrayPreguntas= new Array();
+ export function mostrarPreguntas(pregunta){ //Muestra las preguntas en orden aleatorio además muestra la categoría de cada pregunta.
+  let arrayPreguntas= new Array();
    arrayPreguntas.push(pregunta.correct_answer);
-  
-  let div =  document.createElement("div");
-  /* div.innerHTML=`<h2> ${pregunta.category}</h2><p>${pregunta.question}</p>
-  <input type="radio" id="correct" name="pregunta" value="${pregunta.correct_answer}">
-  <label for="correct">${pregunta.correct_answer}</label>`; */
+ // console.log(pregunta.correct_answer); Para probar las correctas
+  let div = document.getElementById("mostrar");
+  div.classList.remove("hidden")
+  div.innerHTML+=`<h2> ${pregunta.category}</h2><p>${pregunta.question}</p>`;
+
   pregunta.incorrect_answers.forEach(incorrecta => {
     arrayPreguntas.push(incorrecta);
-    /* div.innerHTML+=`<input type="radio" class="false" name="pregunta" >
-    <label for="pregunta">${incorrecta}</label>`; */
+   
   });
-console.log(arrayPreguntas);
-  //insertar una función para mezclar las preguntas
+  let posicion=Math.round(Math.random()*3);
   
+  for(let i= 0; i<4;i++){//Así conseguimos que se pongan las preguntas en orden aleatorio
+    if(posicion>3)     posicion=0; //Para que no nunca se salga del array de 4 posiciones;
+    if(posicion==0){//Sabemos que es la posición de la pregunta correcta
+     div.innerHTML+=` <input type="radio" class="correct" name="${pregunta.question}" value="${arrayPreguntas[posicion]}">
+      <label for="correct">${arrayPreguntas[posicion]}</label>`;
+    }
+    else if(arrayPreguntas[posicion]!= undefined){//Hay preguntas de verdadero o falso así las controlamos ya que esas posiciones serán 
+       div.innerHTML+=`<input type="radio" class="false" name="${pregunta.question}" >
+    <label for="pregunta">${arrayPreguntas[posicion]}</label>`;
+
+    } 
+    posicion++;
+  }
+ 
+
+
   
-  document.body.appendChild(div);
+
+
 } 
+export function comprobarAcertadas(){//Así nos muestra la cantidad de preguntas acertadas y además da estilo a las correctas(verde) y si fallamos nos lo muestra en rojo.
+//  
+
+var contador=0;
+  let acertadas= document.querySelectorAll("input[type='radio'].correct:checked");
+  
+  acertadas.forEach(acertada=>{
+   contador++;
+  });
+  let correctas= document.querySelectorAll(".correct");
+
+  correctas.forEach(acertada=>{
+    acertada.setAttribute("class","verde");
+  });
+  let falladas= document.querySelectorAll("input[type='radio'].false:checked");
+  falladas.forEach(fallada=>{
+ fallada.setAttribute("class","rojo");
+  })
+ 
+  let div = document.getElementById("mostrar");
+  div.innerHTML+=`<h2>Resultado del trivial</h2><p>Has acertado ${contador} preguntas</p>`;
+  
+}
+export function vaciar(){//Volver a los valores iniciales
+  var resultados = document.getElementById("final");
+    var reiniciar = document.getElementById("reiniciar");
+  let div = document.getElementById("mostrar");
+  div.innerHTML=""; 
+  resultados.classList.add("hidden");
+  reiniciar.classList.add("hidden");
+}
