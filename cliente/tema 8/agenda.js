d3248ea204c259cd1b7869aac0acc695be3192c1
Rienirr <antoniobonmati.alu@iespacomolla.es>
2021-12-20 19:57:01 +0100

    listo

--- cliente/tema 8/agenda.js
+++ cliente/tema 8/agenda.js
@@ -11,21 +11,22 @@ window.onload = ()=>{
 let agenda= new Agenda();
        
    buscar.addEventListener("click",(event)=>{//Añadir el formulario buscar.
-
+      console.log("hola");
    },false);
    borrar.addEventListener("click",(event)=>{//Añadir el formulario de borrar.
-
+      console.log("hola");
    },false);
    ordenar.addEventListener("click",(event)=>{//Añadir el formulario de ordenar.
-
+      console.log("hola");
    },false);
    anyadir.addEventListener("click",(event)=>{//Añadir el formulario de añadir.
-         let nombre= document.getElementById("nombre").value;
+        /*  let nombre= document.getElementById("nombre").value;
           let apellido= document.getElementById("apellido").value;
         let  dirreccion= document.getElementById("direccion").value;
         let telf= document.getElementById("telefono");
         agenda.anyadir( new Persona(nombre, apellido,dirreccion,telf));
-        agenda.setItem(agenda.listado);
+        agenda.setItem(agenda.listado); */
+        console.log("hola");
        
    },false);
   
