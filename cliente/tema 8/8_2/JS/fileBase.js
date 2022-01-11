ec7520413030778e515898144b6d593f88e77e5b
Rienirr <antoniobonmati.alu@iespacomolla.es>
2022-01-11 17:40:41 +0100

    listo

--- cliente/tema 8/8_2/JS/fileBase.js
+++ cliente/tema 8/8_2/JS/fileBase.js
@@ -1,1 +1,15 @@
-/*Esta clase se encarga de consultar la base de datos de fileBase */
+/*Esta clase se encarga de proporcionar los datos necesarios para conectarnos a la base de datos */
+"use strict";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4TxXcMo1SY2m3UDH9YNNgZjMo6bVLBkg",
+    authDomain: "lista-de-la-compra-83c82.firebaseapp.com",
+    projectId: "lista-de-la-compra-83c82",
+    storageBucket: "lista-de-la-compra-83c82.appspot.com",
+    messagingSenderId: "1058261646163",
+    appId: "1:1058261646163:web:cac607cbb191770785bfa6"
+  };
+
+export const app = initializeApp(firebaseConfig);
--- cliente/tema 8/8_2/JS/listaDeCompra.js
+++ cliente/tema 8/8_2/JS/listaDeCompra.js
@@ -1,1 +1,36 @@
 /*Se encarga de manejar los datos que vienen de la base de datos correspondientes a la lista de la compra */
+"use strict;"
+import { app } from "./fileBase.js";
+import { crearfila } from "./plantillas.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  onSnapshot,
+  doc,
+  query,
+  where,
+  orderBy,
+  limit,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+window.onload =()=> {
+    
+    const db= getFirestore(app);
+    const productos = collection(db,"Productos");
+    const obtenerProductos = async () => {
+        
+        const productosDelDocumento = await getDocs(productos);
+        document.body.innerHTML+="<table> <th>Nombre </th> <th> Peso</th> <th> Precio</th> <th> Descripcion</th> <th> imagen</th>";
+        productosDelDocumento.docs.map((documento) => {
+            
+         
+            document.body.innerHTML+= crearfila(documento.data().nombre,documento.data().peso,documento.data().precio,documento.data().descripcion, documento.data().imagen);
+           
+        });
+        document.body.innerHTML+="</table>"
+      };
+obtenerProductos();
+   
+    
+    }
