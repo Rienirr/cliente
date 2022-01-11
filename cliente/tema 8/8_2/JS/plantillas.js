a6824b85684ccc686ac7a21d74294af061050488
Rienirr <antoniobonmati.alu@iespacomolla.es>
2022-01-11 17:40:35 +0100

    listo

--- cliente/tema 8/8_2/JS/plantillas.js
+++ cliente/tema 8/8_2/JS/plantillas.js
@@ -1,1 +1,9 @@
 /*Esta biblioteca nos permite crear plantillas que nos generar información necesaria para el cliente. */
+"use strict;"
+export function crearfila(nombre,peso,precio,descripcion,imagen="nada"){
+    return `<tr> <td>${nombre}</td>  <td>${peso}</td> <td>${precio}</td> <td>${descripcion} </td> <td>${imagen} </td> </tr>`;
+}
+function añadirfila(id, fila){
+    let espacioAñadir= document.getElementById(id);
+    espacioAñadir.innerHTML+=fila;
+}
